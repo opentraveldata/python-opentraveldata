5425406067206451736c7337c0979603a059cb9b
Denis Arnaud <denis.arnaud@decathlon.com>
2021-11-14 12:30:06 +0100

    [Release] 0.0.9.post1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='opentraveldata',
-    version='0.0.9',
+    version='0.0.9.post1',
     author='Denis Arnaud',
     author_email='denis.arnaud_pypi@m4x.org',
     description=('''Simple Python wrapper for OpenTravelata (OPTD)'''),
