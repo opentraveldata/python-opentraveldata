1642233c5356f9b9976b5a04f5d9b63aeb6ea8fd
Denis Arnaud <denis.arnaud@decathlon.com>
2023-09-22 18:23:07 +0200

    [Release] 0.0.9.post2

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
+from setuptools import setup
 
-import os, setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='opentraveldata',
-    version='0.0.9.post1',
-    author='Denis Arnaud',
-    author_email='denis.arnaud_pypi@m4x.org',
-    description=('''Simple Python wrapper for OpenTravelata (OPTD)'''),
-    license='MIT',
-    keywords='api python optd opentraveldata package',
-    url='https://github.com/opentraveldata/python-opentraveldata',
-    packages=setuptools.find_packages(),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'python-dateutil',
-        'pytz'
-    ],
-)
+setup()
 
